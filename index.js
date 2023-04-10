7c27af62e0f67c66d6f177f8b7196cbe6abfae38
Bruno Vicente <brunovicente.dev@gmail.com>
2023-04-10 10:45:37 -0300

    Added post request to /tacos

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/tacos", (req, res) => {
   res.send("Get /tacos response");
+});
+
+app.post("/tacos", (req, res) => {
+  const { meat, qty } = req.body;
+  res.send("Post /tacos response");
 });
 
 app.listen(3000, () => {
