e43ab739f5e39e893bbf6055d66b666bd666063a
Bruno Vicente <brunovicente.dev@gmail.com>
2023-04-11 11:29:38 -0300

    Added ejs, index page and comments "DB"

--- index.js
+++ index.js
@@ -1,7 +1,35 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+const comments = [
+  {
+    username: "Todd",
+    comment: "lol thats so funny",
+  },
+  {
+    username: "Skyler",
+    comment: "I like to go birdwatching with my dog",
+  },
+  {
+    username: "Sk8erBoi",
+    comment: "Plz delete you account, Todd",
+  },
+  {
+    username: "onlysayswoof",
+    comment: "woof woof woof",
+  },
+];
+
+app.get("/comments", (req, res) => {
+  res.render("comments/index", { comments });
+});
 
 app.get("/tacos", (req, res) => {
   res.send("Get /tacos response");
@@ -15,3 +43,9 @@ app.post("/tacos", (req, res) => {
 app.listen(3000, () => {
   console.log("On port 3000");
 });
+
+// GET /comments - list all comments
+// POST /comments - create a new comment
+// GET /comments/:id - Get one comment (using ID)
+// PATCH /comments/:id - Update one comment
+// DELETE /comments/:id - Destroy one comment
