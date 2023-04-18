4bc5212c0a46764c731737836efc9b1e0f436dce
Bruno Vicente <brunovicente.dev@gmail.com>
2023-04-18 10:38:32 -0300

    Added method override

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const { v4: uuid } = require("uuid");
+const methodOverride = require("method-override");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(methodOverride("_method"));
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
